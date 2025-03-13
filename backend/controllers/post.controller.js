505eafd3efce01beedc6ca58cca657c851acd64e
kongcs2 <kongcs2@gmail.com>
2025-03-13 11:47:11 -0700

    like post mutation added

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -96,7 +96,11 @@ export const likeUnlikePost = async (req, res) => {
       //unlike
       await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
       await User.updateOne({ _id: userId }, { $pull: { likedPosts: postId } });
-      res.status(200).json({ message: "Post unliked" });
+
+      const updatedLikes = post.likes.filter(
+        (id) => id.toString() !== userId.toString()
+      );
+      res.status(200).json(updatedLikes);
     } else {
       //like post
       post.likes.push(userId);
@@ -109,7 +113,8 @@ export const likeUnlikePost = async (req, res) => {
         type: "like",
       });
       await notification.save();
-      res.status(200).json({ message: "Post liked" });
+      const updatedLikes = post.likes;
+      res.status(200).json(updatedLikes);
     }
   } catch (error) {
     console.log("Error in likeUnlikePost controller: ", error);
